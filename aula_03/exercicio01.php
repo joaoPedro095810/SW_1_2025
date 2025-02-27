4fffa951ae98a09b80f41265b37d9c451e274a25
JoaoFerreira <159969483+joaoPedro095810@users.noreply.github.com>
2025-02-27 11:08:19 -0300

    Add files via upload

---
+++ aula_03/exercicio01.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $mensagem = $_POST['cxmensagem'];
+    
+    echo "Nome digitado: $nome <br> ";
+    echo "Envie sua mensagem: $mensagem <br>";
+
+
+?>
---
+++ aula_03/processa01.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = $_GET['cxnome'];
+    $idade = $_GET['cxidade'];
+    
+    echo "Nome digitado: $nome <br> ";
+    echo "idade digitada: $idade anos de vida! <br>";
+
+
+?>
