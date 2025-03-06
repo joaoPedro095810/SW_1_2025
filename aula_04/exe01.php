256734a76323b901e949611f1ee65b26f24edb7e
JoaoFerreira <159969483+joaoPedro095810@users.noreply.github.com>
2025-03-06 09:12:07 -0300

    Add files via upload

---
+++ aula_04/exe01.php
@@ -0,0 +1,13 @@
+<?php   
+    $nome = $_GET['nome'];
+
+    $nota1 = $_GET['nota1'];
+
+    $nota2 = $_GET['nota2'];
+
+    $nota3 = $_GET['nota3'];
+
+    $media = ( $nota1+$nota2+$nota3/ 3);
+
+        echo "$nome, sua média de notas e $media";
+?>
---
+++ aula_04/exe02.php
@@ -0,0 +1,9 @@
+ <?php   
+    $n1 = $_GET['n1'];
+    $n2 = $_GET['n2'];
+    $n3 = $_GET['n3'];
+
+    $maior = max($n1, $n2, $n3);
+
+    echo "o numero maior é" $maior;
+   ?>
